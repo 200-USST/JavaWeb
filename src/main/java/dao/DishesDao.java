cdd72423bc2c744add1b5333c13b5ded0f79e9ec
rlagofla <miss.pinky.0825@gmail.com>
2023-12-16 01:01:41 +0800

    实现了 CanteenDaoImpl
    有个问题，canteenDao 的 newCanteen 方法确实想要返回一个 Canteen 对象吗？暂时返回 null

--- src/main/java/dao/DishesDao.java
+++ src/main/java/dao/DishesDao.java
@@ -1,11 +1,7 @@
 package dao;
 
-import pojo.Canteen;
-import pojo.Dish;
 
-import java.sql.SQLException;
-import java.util.List;
 
 public interface DishesDao {
-    List<Dish> selectCanteen(Integer canteenID) throws SQLException, ClassNotFoundException;
+
 }
--- src/main/java/dao/impl/CanteenDaoImpl.java
+++ src/main/java/dao/impl/CanteenDaoImpl.java
@@ -3,20 +3,38 @@ package dao.impl;
 import dao.CanteenDao;
 import pojo.Canteen;
 import pojo.User;
+import util.DatabaseHelper;
 
 public class CanteenDaoImpl implements CanteenDao {
+    private final DatabaseHelper DbHelper = new DatabaseHelper();
     @Override
     public Canteen newCanteen(Canteen canteen) {
+        DbHelper.update(
+                "insert into canteen (canteenName, canteenLocation, canteenAbstract) values (?, ?, ?)",
+                canteen.getCanteenName(), canteen.getCanteenLocation(), canteen.getCanteenAbstract()
+        );
         return null;
     }
 
     @Override
     public String findCanteenName(User user) {
+        var result = DbHelper.query(
+                "select canteenName from canteen_manager where managerName = ?",
+                user.getUserName()
+        );
+        if (!result.isEmpty()) {
+            return (String) result.get(0).get(0);
+        }
         return null;
     }
 
     @Override
     public boolean isNameExist(String canteenName) {
-        return false;
+        var result = DbHelper.query(
+                "select * from canteen where binary canteenName = ?",
+                canteenName
+        );
+
+        return !result.isEmpty();
     }
 }
