996f0c0758bdec69077baa35cf3a1fa6cc6c386f
PinG <1552014195@qq.com>
2023-12-21 18:23:23 +0800

    提交新Dao需求修改版本

--- src/main/java/dao/DishDao.java
+++ src/main/java/dao/DishDao.java
@@ -8,6 +8,6 @@ import java.util.ArrayList;
 public interface DishDao {
     public ArrayList<Dish> queryAllDishes();//查询所有菜品列表
     public Dish newDish(Dish dish);//新建菜品
-    public boolean isNameExist(String dishName,String canteenName);//查询菜品名字是否已经存在
+    public boolean isNameExist(String dishName,Integer canteenId);//查询菜品名字是否已经存在
     public void modifyDish(Dish dish);
 }
